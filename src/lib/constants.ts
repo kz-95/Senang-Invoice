404f5c74acc4e683f697021f75060c486438fdae
kz-95 <zen95.kz@gmail.com>
2026-06-21 22:21:09 +0800

    fix: default APK_DOWNLOAD_URL to /senang-invoice.apk so download button is always enabled

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const GENERAL_TIN = 'EI00000000010'
 export const CONSOLIDATED_CODE = '004'
 export const APP_NAME = 'Senang Inv'
 
-export const APK_DOWNLOAD_URL = process.env.NEXT_PUBLIC_APK_URL ?? ''
+export const APK_DOWNLOAD_URL = process.env.NEXT_PUBLIC_APK_URL || '/senang-invoice.apk'
 export const PLAYSTORE_URL = process.env.NEXT_PUBLIC_PLAYSTORE_URL ?? ''
 export const PLAYSTORE_COMING_SOON = true
 
